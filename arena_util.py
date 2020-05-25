8ee635d2db167790997c46a66e46ffc97781181e
sjPark <sungjun3010@gmail.com>
2020-05-25 16:20:30 +0900

    arena_util code 수정사항 반영

--- arena_util.py
+++ arena_util.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import io
 import os
 import json
@@ -11,13 +10,13 @@ import numpy as np
 
 def write_json(data, fname):
     def _conv(o):
-        if isinstance(o, np.int64):
+        if isinstance(o, (np.int64, np.int32)):
             return int(o)
         raise TypeError
 
     parent = os.path.dirname(fname)
     distutils.dir_util.mkpath("./arena_data/" + parent)
-    with io.open("./arena_data/" + fname, "w", encoding="utf8") as f:
+    with io.open("./arena_data/" + fname, "w", encoding="utf-8") as f:
         json_str = json.dumps(data, ensure_ascii=False, default=_conv)
         f.write(json_str)
 
@@ -46,5 +45,3 @@ def most_popular(playlists, col, topk_count):
 
     topk = c.most_common(topk_count)
     return c, [k for k, v in topk]
-
-  
