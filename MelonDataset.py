066bff6c215457deb59226506cca2cf29f3d8a59
sungjun3010 <sungjun3010@gmail.com>
2020-07-23 20:02:31 +0900

    genre 사용한 버전, 사용 안한 버전 구분해서 함수, 클래스 나눔 -user_genre로 사용할지, 안할지 결정

--- MelonDataset.py
+++ MelonDataset.py
@@ -13,6 +13,46 @@ class SongTagDataset(Dataset):
         self.prep_song2id = dict(np.load(prep_song2id_file_path, allow_pickle=True).item())
         self.num_songs = len(self.prep_song2id)
         self.num_tags = len(self.tag2id)
+
+    def __len__(self):
+        return len(self.train)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        _id = self.train[idx]['id']
+        song_vector = self._song_ids2vec(self.train[idx]['songs'])
+        tag_vector = self._tag_ids2vec(self.train[idx]['tags'])
+        _input = torch.from_numpy(
+            np.concatenate([song_vector, tag_vector]).astype(np.float32))
+
+        return _id, _input
+
+    def _song_ids2vec(self, songs):
+        songs = [self.prep_song2id[song] for song in songs if song in self.prep_song2id.keys()]
+
+        songs = np.asarray(songs, dtype=np.int)
+        bin_vec = np.zeros(self.num_songs)
+        if len(songs) > 0:
+            bin_vec[songs] = 1
+        return np.array(bin_vec)
+
+    def _tag_ids2vec(self, tags):
+        tags = [self.tag2id[tag] for tag in tags if tag in self.tag2id.keys()]
+        tags = np.asarray(tags, dtype=np.int)
+        bin_vec = np.zeros(self.num_tags)
+        bin_vec[tags] = 1
+        return np.array(bin_vec)
+
+
+class SongTagGenreDataset(Dataset):
+    def __init__(self, data_file_path, tag2id_file_path, prep_song2id_file_path):
+        self.train = load_json(data_file_path)
+        self.tag2id = dict(np.load(tag2id_file_path, allow_pickle=True).item())
+        self.prep_song2id = dict(np.load(prep_song2id_file_path, allow_pickle=True).item())
+        self.num_songs = len(self.prep_song2id)
+        self.num_tags = len(self.tag2id)
         self._init_song_meta()
 
     def __len__(self):
--- train.py
+++ train.py
@@ -5,18 +5,23 @@ import argparse
 import torch.nn as nn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from MelonDataset import SongTagDataset
+from MelonDataset import SongTagDataset, SongTagGenreDataset
 from Models import AutoEncoder
 from data_util import *
 from arena_util import write_json
 from evaluate import ArenaEvaluator
 
 
-def train(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path):
+def train_with_genre(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     id2tag_dict = dict(np.load(id2tag_file_path, allow_pickle=True).item())
     id2prep_song_dict = dict(np.load(id2prep_song_file_path, allow_pickle=True).item())
+
+    qestion_data_loader = None
+    if not submit:
+        qestion_data_loader = DataLoader(question_dataset, shuffle=True, batch_size=batch_size,
+                                         num_workers=num_workers)
 
     # parameters
     num_songs = train_dataset.num_songs
@@ -67,32 +72,112 @@ def train(train_dataset, id2prep_song_file_path, id2tag_file_path, question_data
         print('loss: %d %d%% %.4f' % (epoch, epoch / epochs * 100, running_loss))
 
         torch.save(model, model_file_path)
-        if epoch == epochs-1:
-            if not submit:
-                qestion_data_loader = DataLoader(question_dataset, shuffle=True, batch_size=batch_size,
-                                                 num_workers=num_workers)
-                if epoch % testevery == 0:
-                    elements = []
-                    for idx, (_id, _data) in enumerate(tqdm(qestion_data_loader, desc='testing...')):
-                        with torch.no_grad():
-                            _data = _data.to(device)
-                            output = model(_data)
+        if not submit:
+            if epoch % testevery == 0:
+                elements = []
+                for idx, (_id, _data) in enumerate(tqdm(qestion_data_loader, desc='testing...')):
+                    with torch.no_grad():
+                        _data = _data.to(device)
+                        output = model(_data)
 
-                            songs_tags_input, _ = torch.split(_data, num_songs + num_tags, dim=1)
-                            songs_input, tags_input = torch.split(songs_tags_input, num_songs, dim=1)
-                            songs_and_tags_output, _ = torch.split(output, num_songs + num_tags, dim=1)
-                            songs_output, tags_output = torch.split(songs_and_tags_output, num_songs, dim=1)
-                            songs_ids = binary_songs2ids(songs_input, songs_output, id2prep_song_dict)
-                            tag_ids = binary_tags2ids(tags_input, tags_output, id2tag_dict)
+                        songs_tags_input, _ = torch.split(_data, num_songs + num_tags, dim=1)
+                        songs_input, tags_input = torch.split(songs_tags_input, num_songs, dim=1)
+                        songs_and_tags_output, _ = torch.split(output, num_songs + num_tags, dim=1)
+                        songs_output, tags_output = torch.split(songs_and_tags_output, num_songs, dim=1)
 
-                            _id = list(map(int, _id))
-                            for i in range(len(_id)):
-                                element = {'id': _id[i], 'songs': list(songs_ids[i]), 'tags': tag_ids[i]}
-                                elements.append(element)
+                        songs_ids = binary_songs2ids(songs_input, songs_output, id2prep_song_dict)
+                        tag_ids = binary_tags2ids(tags_input, tags_output, id2tag_dict)
 
-                    write_json(elements, temp_fn)
-                    evaluator.evaluate(answer_file_path, temp_fn)
-                    os.remove(temp_fn)
+                        _id = list(map(int, _id))
+                        for i in range(len(_id)):
+                            element = {'id': _id[i], 'songs': list(songs_ids[i]), 'tags': tag_ids[i]}
+                            elements.append(element)
+
+                write_json(elements, temp_fn)
+                evaluator.evaluate(answer_file_path, temp_fn)
+                os.remove(temp_fn)
+
+
+def train_without_genre(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    id2tag_dict = dict(np.load(id2tag_file_path, allow_pickle=True).item())
+    id2prep_song_dict = dict(np.load(id2prep_song_file_path, allow_pickle=True).item())
+
+    qestion_data_loader = None
+    if not submit:
+        qestion_data_loader = DataLoader(question_dataset, shuffle=True, batch_size=batch_size,
+                                         num_workers=num_workers)
+
+    # parameters
+    num_songs = train_dataset.num_songs
+    num_tags = train_dataset.num_tags
+
+    # hyper parameters
+    D_in = D_out = num_songs + num_tags
+
+    evaluator = ArenaEvaluator()
+    data_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+
+    model = AutoEncoder(D_in, H, D_out, dropout=dropout).to(device)
+
+    testevery = 5
+
+    parameters = model.parameters()
+    loss_func = nn.BCELoss()
+    optimizer = torch.optim.Adam(parameters, lr=learning_rate)
+
+    try:
+        model = torch.load(model_file_path)
+        print("\n--------model restored--------\n")
+    except:
+        print("\n--------model not restored--------\n")
+        pass
+
+    temp_fn = 'arena_data/answers/temp.json'
+    if os.path.exists(temp_fn):
+        os.remove(temp_fn)
+
+    for epoch in range(epochs):
+        print()
+        print('epoch: ', epoch)
+        running_loss = 0.0
+        for idx, (_id, _data) in enumerate(tqdm(data_loader, desc='training...')):
+            _data = _data.to(device)
+
+            optimizer.zero_grad()
+            output = model(_data)
+            loss = loss_func(output, _data)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print('loss: %d %d%% %.4f' % (epoch, epoch / epochs * 100, running_loss))
+
+        torch.save(model, model_file_path)
+        if not submit:
+            if epoch % testevery == 0:
+                elements = []
+                for idx, (_id, _data) in enumerate(tqdm(qestion_data_loader, desc='testing...')):
+                    with torch.no_grad():
+                        _data = _data.to(device)
+                        output = model(_data)
+
+                        songs_input, tags_input = torch.split(_data, num_songs, dim=1)
+                        songs_output, tags_output = torch.split(output, num_songs, dim=1)
+
+                        songs_ids = binary_songs2ids(songs_input, songs_output, id2prep_song_dict)
+                        tag_ids = binary_tags2ids(tags_input, tags_output, id2tag_dict)
+
+                        _id = list(map(int, _id))
+                        for i in range(len(_id)):
+                            element = {'id': _id[i], 'songs': list(songs_ids[i]), 'tags': tag_ids[i]}
+                            elements.append(element)
+
+                write_json(elements, temp_fn)
+                evaluator.evaluate(answer_file_path, temp_fn)
+                os.remove(temp_fn)
 
 
 if __name__ == "__main__":
@@ -103,7 +188,8 @@ if __name__ == "__main__":
     parser.add_argument('-learning_rate', type=float, help="learning rate", default=0.0005)
     parser.add_argument('-dropout', type=float, help="dropout", default=0.2)
     parser.add_argument('-num_workers', type=int, help="num workers", default=20)
-    parser.add_argument('-prep_method_thr', type=float, help="frequency threshold", default=2)
+    parser.add_argument('-freq_thr', type=float, help="frequency threshold", default=2)
+    parser.add_argument('-use_genre', type=float, help="whether use genre or not, yes: 1, no: 0", default=0)
     parser.add_argument('-submit', type=int, help="arena_data/orig: 0 res: 1", default=0)
 
     args = parser.parse_args()
@@ -115,7 +201,8 @@ if __name__ == "__main__":
     learning_rate = args.learning_rate
     dropout = args.dropout
     num_workers = args.num_workers
-    prep_method_thr = args.prep_method_thr
+    freq_thr = args.freq_thr
+    use_genre = args.use_genre
     submit = args.submit
 
     if submit:
@@ -134,18 +221,23 @@ if __name__ == "__main__":
     tag2id_file_path = f'{default_file_path}/t_tag2id.npy'
     id2tag_file_path = f'{default_file_path}/t_id2tag.npy'
 
-    prep_song2id_file_path = f'{default_file_path}/t_freq_song2id_thr{prep_method_thr}.npy'
-    id2prep_song_file_path = f'{default_file_path}/t_id2freq_song_thr{prep_method_thr}.npy'
+    prep_song2id_file_path = f'{default_file_path}/t_freq_song2id_thr{freq_thr}.npy'
+    id2prep_song_file_path = f'{default_file_path}/t_id2freq_song_thr{freq_thr}.npy'
 
     if not (os.path.exists(tag2id_file_path) & os.path.exists(id2tag_file_path)):
         tags_ids_convert(train_file_path, tag2id_file_path, id2tag_file_path)
 
-    model_file_path = 'model/autoencoder_{}_{}_{}_{}_{}{}_gnr_t.pkl'.\
-        format(H, batch_size, learning_rate, dropout, prep_method_thr, model_postfix)
-
-    train_dataset = SongTagDataset(train_file_path, tag2id_file_path, prep_song2id_file_path)
-    question_dataset = SongTagDataset(question_file_path, tag2id_file_path, prep_song2id_file_path)
-
-    train(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path)
+    if use_genre:
+        train_dataset = SongTagGenreDataset(train_file_path, tag2id_file_path, prep_song2id_file_path)
+        question_dataset = SongTagGenreDataset(question_file_path, tag2id_file_path, prep_song2id_file_path)
+        model_file_path = 'model/autoencoder_{}_{}_{}_{}_{}{}_gnr_t.pkl'. \
+            format(H, batch_size, learning_rate, dropout, freq_thr, model_postfix)
+        train_with_genre(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path)
+    else:
+        train_dataset = SongTagDataset(train_file_path, tag2id_file_path, prep_song2id_file_path)
+        question_dataset = SongTagDataset(question_file_path, tag2id_file_path, prep_song2id_file_path)
+        model_file_path = 'model/autoencoder_{}_{}_{}_{}_{}{}_t.pkl'. \
+            format(H, batch_size, learning_rate, dropout, freq_thr, model_postfix)
+        train_without_genre(train_dataset, id2prep_song_file_path, id2tag_file_path, question_dataset, answer_file_path, model_file_path)
 
     print('train completed')
