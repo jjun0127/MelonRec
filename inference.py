fa862938c908d12575f427c22f721e14de422089
wonchang24 <32264689+wonchang24@users.noreply.github.com>
2020-07-28 18:29:42 +0900

    Add files via upload

--- inference.py
+++ inference.py
@@ -6,6 +6,7 @@ from get_w2v_scores import get_w2v_scores
 import argparse
 from recommender import Recommender
 import numpy as np
+import os
 
 sim_measure = 'cos'
 n_msp = 50
@@ -15,7 +16,8 @@ freq_thr = 2
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-mode', type=int, help="local_val: 0, val: 1, test: 2", default=2)
-    parser.add_argument('-retrain', type=int, help="remove tokenizer&w2v model and retrain 1: True, 0: False", default=0)
+    parser.add_argument('-retrain', type=int, help="remove tokenizer&w2v model and retrain 1: True, 0: False",
+                        default=0)
     args = parser.parse_args()
     _submit_type = args.mode
     _retrain = args.retrain
@@ -31,6 +33,8 @@ if __name__ == '__main__':
         train_data = load_json(train_file_path)
         question_data = load_json(question_file_path)
         model_file_path = "model/autoencoder_450_256_0.0005_0.2_2_local_val.pkl"
+        auto_score_file_path = "scores/local_val_scores_bias_cos"
+        w2v_score_file_path = 'scores/local_val_scores_title_cos_24000'
 
     elif _submit_type == 1:
         default_file_path = 'res'
@@ -41,6 +45,8 @@ if __name__ == '__main__':
         train_data = load_json(train_file_path) + load_json(val_file_path)
         question_data = load_json(val_file_path)
         model_file_path = "model/autoencoder_450_256_0.0005_0.2_2_val.pkl"
+        auto_score_file_path = "scores/val_scores_bias_cos"
+        w2v_score_file_path = 'scores/val_scores_title_cos_24000'
 
     elif _submit_type == 2:
         default_file_path = 'res'
@@ -49,9 +55,12 @@ if __name__ == '__main__':
         train_file_path = f'{default_file_path}/train.json'
         val_file_path = f'{default_file_path}/val.json'
         test_file_path = f'{default_file_path}/test.json'
-        train_data = load_json(train_file_path) + load_json(val_file_path) + load_json(val_file_path) + load_json(test_file_path)
+        train_data = load_json(train_file_path) + load_json(val_file_path) + load_json(val_file_path) + load_json(
+            test_file_path)
         question_data = load_json(test_file_path)
         model_file_path = "model/autoencoder_450_256_0.0005_0.2_2_test.pkl"
+        auto_score_file_path = "scores/test_scores_bias_cos"
+        w2v_score_file_path = 'scores/test_scores_title_cos_24000'
 
     else:
         print('mode error! local_val: 0, val: 1, test: 2')
@@ -64,13 +73,21 @@ if __name__ == '__main__':
     prep_song2id_file_path = f'{default_file_path}/freq_song2id_thr{freq_thr}_{model_postfix}.npy'
     id2prep_song_file_path = f'{default_file_path}/id2freq_song_thr{freq_thr}_{model_postfix}.npy'
 
-    get_autoencoder_scores(model_file_path, model_postfix)
-    get_w2v_scores(model_postfix, _retrain)
+    if not os.path.exists(model_file_path):
+        print("Error: there is no autoencoder model. Please execute train.py first")
+        sys.exit(1)
+    if _retrain:
+        get_autoencoder_scores(model_file_path, model_postfix)
+        get_w2v_scores(model_postfix, _retrain)
+    else:
+        if (not os.path.exists(auto_score_file_path + '.npy')) or (not os.path.exists(auto_score_file_path + '_gnr.npy')):
+            get_autoencoder_scores(model_file_path, model_postfix)
+        if not os.path.exists(w2v_score_file_path + '.npy'):
+            get_w2v_scores(model_postfix, _retrain)
 
     song_meta = load_json('res/song_meta.json')
     prep_song2id = dict(np.load(prep_song2id_file_path, allow_pickle=True).item())
     freq_song = set(prep_song2id.keys())
 
-    rec_list = Recommender(train_data, question_data, n_msp, n_mtp, model_postfix, sim_measure, song_meta, freq_song, save=True)
-
-
+    rec_list = Recommender(train_data, question_data, n_msp, n_mtp, model_postfix, sim_measure, song_meta, freq_song,
+                           save=True)
